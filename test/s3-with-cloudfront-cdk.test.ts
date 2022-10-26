1045e0b89b1d32d80337ff70e925de1c5238f128
Michael Liendo <mtliendo@gmail.com>
2022-10-25 22:51:42 -0500

    Initial commit

---
+++ test/s3-with-cloudfront-cdk.test.ts
@@ -0,0 +1,17 @@
+// import { Template } from '@aws-cdk/assertions';
+// import * as cdk from '@aws-cdk/core';
+// import * as S3WithCloudfrontCdk from '../lib/s3-with-cloudfront-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/s3-with-cloudfront-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new S3WithCloudfrontCdk.S3WithCloudfrontCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
